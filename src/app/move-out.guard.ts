539529c472d01a0c64e59fb724a50b959580b941
LaxmanKumarP <laxmanncde@gmail.com>
2024-08-26 12:53:49 -0400

    removed console

--- src/app/move-out.guard.ts
+++ src/app/move-out.guard.ts
@@ -4,7 +4,6 @@ import { CanDeactivateFn, Router } from '@angular/router';
 export const moveOutGuard: CanDeactivateFn<unknown> = (currentRoute, currentState) => {
   // const router = inject(Router);
 
-  console.log('check the guard!!!!!');
   alert('checkkkkkkkkk meeeeeeeee')
   return true;
 };
